856d98bfe6cd9a1dea5d8274194d0d563fa5d9d1
sumanta90 <79313911+sumanta90@users.noreply.github.com>
2021-02-25 09:48:39 +0530

    Router refined

--- src/components/add_queue.js
+++ src/components/add_queue.js
@@ -1,12 +1,69 @@
 import { Component } from "react";
 
 
-class AddQueue extends Component{
+
+//form for add queue
+class AddQueueForm extends Component{
+    render(){
+        return(
+            
+            <div className="fluid">
+
+                <div style={{  }}>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Amb. ID</label>
+                        <input type="text" placeholder="Ambassador ID" />
+                    </div>
+                    <div className="field">
+                        <label>Queue. ID</label>
+                        <input type="text" placeholder="Queue ID" />
+                    </div>
+                    <div className="field">
+                        <label>Task Count</label>
+                        <input type="text" placeholder="Task Count" />
+                    </div>
+                    <div className="field">
+                        <label>Reason</label>
+                        <input type="text" placeholder="Reason / Note" />
+                    </div>
+                    <div className="field">
+                        <input type="submit" className="fluid ui teal button" value="Submit" />
+                    </div>
+                </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+class AddQueueRoot extends Component{
     render(){
         return (
-
-            <div>
-                <h1>Add queue page</h1>
+            <div className="">
+                <div className="ui grid container">
+                    <div className="two column row">
+                        
+                        <div className="column ui segment">
+                        <h1 class="ui header">Semantic UI Fixed Template</h1>
+                            <AddQueueForm />
+                        </div>
+                        <div className="column"></div>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
+}
+class AddQueue extends Component{
+    
+    render(){
+        return (
+            
+            <div className="ui container">
+                <h1 class="ui header">Semantic UI Fixed Template</h1>
+                <AddQueueRoot />
             </div>
         )
     }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
 
+
 class MyNavBar extends Component{
     render(){
         return (
-            <div className="ui inverted menu">
+            <div className="ui fixed inverted menu">
                 <div className="header item">21North</div>
                 <div className="active item"><a href="/add-queue">Add Queue</a></div>
                 <a className="item"><a href="/queue-list">Queue List</a></a>
